fc476bb9c0f061ce531079a5df3f38709c309a8a
andfaulkner <andfaulkner@gmail.com>
2016-12-12 23:00:17 -0500

    added more filtering functionality

--- index.js
+++ index.js
@@ -1,22 +1,129 @@
+const isArray = require('lodash.isarray');
+const isObject = require('lodash.isobject');
 const reject = require('lodash.reject');
+const isEqual = require('lodash.isequal');
 
-function isAssignmentArg(arg) {
-    return arg.match(/^-?[^-]+=.+$/);
+const defaultOpts = {
+    // options: true | false
+    keepLonelyDashes: false,
+    // options: 'all' | 'none' | 'noflag'
+    assignments: 'all',
 }
 
-function startsWithDash(arg) {
-    return arg.match(/^--?[^-]+/gim);
+
+/********************************* MODULE LOGIC HELPER FUNCTIONS **********************************/
+/**
+ * Returns false for any arg containing an =
+ * @param  {string} arg - CLI argument string - check for =
+ */
+const isAnyAssignmentArg = (arg) => {
+    return !!arg.match(/.*=.*/g);
+}
+
+const isAssignmentFlagArg = (arg) => {
+    return !!arg.match(/^--?(([a-zA-Z0-9_]+.*)|("[a-zA-Z0-9_]+.*")|('[a-zA-Z0-9_]+.*'))=.+$/);
+}
+
+const notAssignmentFlagArg = (arg) => {
+    return !isAssignmentFlagArg(arg);
+}
+
+const startDashesThenNonDashes = (arg) => {
+    return !!arg.match(/^--?[a-zA-Z0-9_]+/gim);
+}
+
+const startsWithDash = (arg) => {
+    return !!arg.match(/^-/gim);
+}
+
+const isStandaloneDashes = (arg) => {
+    return !!arg.match(/^--?-?$/gim);
+}
+
+/******************** MODULE SCAFFOLDING HELPERS (ARGUMENTS, VALIDATION, ETC.) ********************/
+/**
+ * Remove dashes, underscores, and uppercase values from options, and removes trailing "s".
+ * (Note: sort of a dirty solution, but I was feeling lazy)
+ *
+ * @param  {String} opt - option string
+ * @return {String} cleaned option string
+ */
+function cleanOpt(opt) {
+    return opt.toLowerCase().split('-').join('').split('_').join('').split(/s$/mi).join('')
 }
 
 /**
- * Exclude all 'flags' from list of arguments passed to a script via the terminal
- * @param  {Array<String>} procArgs - cli args list {OPTIONAL; defaults to process.argv}
- * @return {Array<String}  cli args with flags excluded
+ * Validate options object. Throw if invalid
+ * @param  {Object} opts
  */
-const cliArgsMinusFlags = (procArgs = process.argv) => {
-    return reject(procArgs, (arg, idx, procArgsOrig) => {
-        return (startsWithDash(arg) && !(isAssignmentArg(arg)));
-    });
+function validateOpts(opts) {
+    switch(cleanOpt(opts.assignments)) {
+        case "all":
+        case "noflag":
+        case "none":
+            return 'ok';
+        default:
+            throw new TypeError(
+                'opts.assignments must be a string equal to "all", "none", or "noFlags"'
+            );
+    }
 }
 
-module.exports = cliArgsMinusFlags;
+const isOptsInArgOneToFilterArgv = (argvs, opts) => {
+    return ((!isArray(argvs)) && (isObject(argvs)) && (isEqual(opts, defaultOpts)));
+}
+
+/**
+ * Alter and reposition arguments passed to filteredArgv if needed.
+ * If options object given but no cli arguments list, reassign options arg to receive
+ * content of first argument (argvs).
+ * Clean the value of assignments option.
+ *
+ * @param  {Array | Object} argvs
+ * @param  {Object} opts
+ * @return {Object}
+ */
+function normalizefilteredArgvArguments(argvs, opts) {
+    // reassign accordingly if only options were passed in
+    if (isOptsInArgOneToFilterArgv(argvs, opts)) {
+        opts = Object.assign({}, defaultOpts, argvs);
+        argvs = process.argv;
+    }
+    validateOpts(opts);
+    opts.assignments = cleanOpt(opts.assignments);
+    return { cleanArgvs: argvs, cleanOpts: opts };
+}
+
+/***************************************** MAIN FUNCTION ******************************************/
+/**
+ * Exclude all 'flags' from list of arguments passed to a script via the terminal
+ * @param  {Array<String>} argvs - cli args list {OPTIONAL; defaults to process.argv}
+ * @param  {Object} opts - options object
+ *
+ * @return {Array<String}  cli args with flags excluded
+ */
+const filteredArgv = (argvs = process.argv, opts = defaultOpts) => {
+    const { cleanArgvs, cleanOpts } = normalizefilteredArgvArguments(argvs, opts);
+
+    const output = reject(cleanArgvs, (arg) => {
+        const startDashPlusOther = startDashesThenNonDashes(arg);
+        if (cleanOpts.assignments === 'all' && isAnyAssignmentArg(arg)) return false;
+        if ((cleanOpt(cleanOpts.assignments) === 'noflag') && isAssignmentFlagArg(arg)) return true;
+        if ((cleanOpts.assignments === 'none') && isAnyAssignmentArg(arg)) return true;
+        if (isStandaloneDashes(arg)) return !cleanOpts.keepLonelyDashes;
+        if (startDashesThenNonDashes(arg)) return true;
+    });
+    return output;
+}
+
+/********************************************* EXPORT *********************************************/
+
+module.exports = (process.env.__FILTER_ARGV_MOCHA_MODULE_TESTING__)
+    ? {
+        filteredArgv,
+        startDashesThenNonDashes,
+        startsWithDash,
+        isAssignmentFlagArg,
+        isStandaloneDashes
+    } 
+    : { filteredArgv }
--- test/index.js
+++ test/index.js
@@ -1,43 +1,132 @@
 // Store original process.argv
 const oldProcArgs = Object.assign({}, process.argv);
 
+process.env.__FILTER_ARGV_MOCHA_MODULE_TESTING__ = true;
+
 /************************************** THIRD-PARTY IMPORTS ***************************************/
 const { expect } = require('chai');
 const mocha = require('mocha');
 
 /*************************************** TESTED FILE IMPORT ***************************************/
-const cliArgsMinusFlags = require('../index');
+const { filteredArgv, startDashesThenNonDashes,
+        startsWithDash, isAssignmentFlagArg, isStandaloneDashes } = require('../index');
 
 /********************************************* TESTS **********************************************/
 describe('index.js', function() {
     it('index.js exists', function() {
-      expect(cliArgsMinusFlags).to.exist;
+      expect(filteredArgv).to.exist;
     });
 
-    describe('cliArgsMinusFlags', function() {
-        it('exists', function () { expect(cliArgsMinusFlags).to.exist });
+    describe('startDashesThenNonDashes', function() {
+        it('is true for string starting with -- or - followed by other chars', function () {
+            ['--asdf', '--1v4f', '-vtrb', '-Z', '-a', '-1', '--5', '-43'].forEach((arg) => {
+                expect(startDashesThenNonDashes(arg)).to.be.true;
+            });
+            ['--', '-', '', 'ege', 'erg'].forEach((arg) => {
+                expect(startDashesThenNonDashes(arg)).to.be.false;
+            })
+        });
+    });
+
+    describe('startsWithDash', function() {
+        it('is true for items that start with dash, false for items that do not', function() {
+            ['--asd', '--1f4', '-VT', '-Z', '-a', '-1', '--5', '-43', '--', '-'].forEach((arg) => {
+                expect(startsWithDash(arg)).to.be.true;
+            });
+            [
+                'asd', '54g5', '!!R#F', 'br', '', '<div>aerf</div>', 'uge-ar', '44------t', 'a--'
+            ].forEach((arg) => {
+                    expect(startsWithDash(arg)).to.be.false;
+                });
+        });
+    });
+
+    describe('isAssignmentFlagArg', function() {
+        it('is true for all items w >= 1 alphanumeric char or _ at any point before an =, unless ' +
+                'the pre-= text is quoted & the surrounding quote types don\'t match', function() {
+            [ '--gr=argh', '--sig=-', '--v=things',
+              '--a-v-d=ok-ya', '--"erg"=fgre', "--'erg'=fgre"
+            ].forEach((arg) => {
+                expect(isAssignmentFlagArg(arg)).to.be.true;
+            });
+            [ 'fw', 'btb', '', '4345345', '=', '-=', '-=-', '=gegr',
+              '==ergeg', '--==awga', `--'erg"=fgre`
+            ].forEach((arg) => {
+                expect(isAssignmentFlagArg(arg)).to.be.false;
+            });
+        });
+    });
+
+    describe('isStandaloneDashes', function() {
+        it('is true for args consisting of 1, 2 or 3 standalone dashes & nothing else', function() {
+            ['---', '--', '--'].forEach((arg) => {
+                expect(isStandaloneDashes(arg)).to.be.true;
+            });
+            ['', '--eraesg', '124', '--wef=erg--', '--grf=a2#@#--g--', '3', '"'].forEach((arg) => {
+                expect(isStandaloneDashes(arg)).to.be.false;
+            });
+        })
+    });
+
+    describe('filteredArgv', function() {
+        it('exists', function () { expect(filteredArgv).to.exist });
 
         it('does not exclude items not prefixed with - or -- from arrays given', function() {
-            expect(cliArgsMinusFlags(['asdf'])).to.eql(['asdf']);
+            expect(filteredArgv(['asdf'])).to.eql(['asdf']);
         });
 
         it('excludes items prefixed with - or -- from arrays', function() {
             const testArray1 = ['asdf', '--ok', '-v', 'ntyo'];
-            expect(cliArgsMinusFlags(testArray1)).to.eql(['asdf', 'ntyo']);
+            expect(filteredArgv(testArray1)).to.eql(['asdf', 'ntyo']);
         });
 
         it('defaults to using process.argv as arrayList if no argument given', function () {
             process.argv = ['argOne', 'argTwo', 'argThree'];
-            expect(cliArgsMinusFlags()).to.eql(process.argv);
+            expect(filteredArgv()).to.eql(process.argv);
 
             process.argv = ['argOne', 'argTwo', '--flag', 'argThree'];
-            expect(cliArgsMinusFlags()).to.eql(['argOne', 'argTwo', 'argThree']);
+            expect(filteredArgv()).to.eql(['argOne', 'argTwo', 'argThree']);
    
             process.argv = Object.assign({}, oldProcArgs); // restore args
         });
+
+        it('keeps args w/ an = preceded & followed by at least 1 non-dash char', function () {
+            process.argv = ['argOne', 'argTwo', 'argThree', '--name=my-component'];
+            expect(filteredArgv()).to.eql(process.argv);
+            process.argv = Object.assign({}, oldProcArgs); // restore args
+        });
+
+        it('by default excludes standalone -- or -', function () {
+            process.argv = ['--', '-'];
+            expect(filteredArgv()).to.eql([]);
+            process.argv = Object.assign({}, oldProcArgs); // restore args
+        });
+
+        it('if requested, can keep standalone -- or -', function () {
+            process.argv = ['--', '-'];
+            expect(filteredArgv({ keepLonelyDashes: true })).to.eql(['--', '-']);
+            expect(filteredArgv({ keepLonelyDashes: false })).to.be.empty;
+
+            process.argv = ['asdf', '--', '-', '--v'];
+            expect(filteredArgv({ keepLonelyDashes: true })).to.eql(['asdf', '--', '-']);
+
+            process.argv = Object.assign({}, oldProcArgs); // restore args
+        });
+
+        it('can optionally exclude assignment args e.g. --name=asdf <-default included', function () {
+            process.argv = ['--name=meeka', 'name=meeka'];
+            expect(filteredArgv({ assignments: 'all' })).to.eql(['--name=meeka', 'name=meeka']);
+            expect(filteredArgv({ assignments: 'noFlags' })).to.eql(['name=meeka']);
+            expect(filteredArgv({ assignments: 'none' })).to.be.empty;
+
+
+            process.argv = Object.assign({}, oldProcArgs); // restore args
+        });
+
+
     });
+
 });
 
 // Restore original process.argv
 process.argv = Object.assign({}, oldProcArgs);
-
